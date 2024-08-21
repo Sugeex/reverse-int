3876beec4ae50b48520bfda6c67cfd7d12440422
Daniil Nemerchenko <Nemerchenko02@mail.ru>
2024-08-21 23:20:22 +0300

    ez

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   let a = n.toString();
+    let result = '';
+    for (let i=a.length-1; i>=0;i--)
+    {
+        result += a[i];
+    }
+    console.log(result);
+    return parseInt(result);
 }
